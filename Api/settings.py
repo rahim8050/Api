a0e853fa0b76df0f079a0fdbc25bb8ca73e73804
rahim8050 <rahimranxx8050@gmail.com>
2025-06-09 16:45:48 +0300

    remove commented-out imports in settings and urls files

--- Api/settings.py
+++ Api/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'api.apps.ApiConfig',
     'users.apps.UsersConfig',
     'rest_framework',
     'rest_framework_simplejwt',
